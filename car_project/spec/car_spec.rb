4f65fd9d6df78e58be9f2ef1223e8d29004ccf94
kot <miseinen22@gmail.com>
2021-05-07 05:12:01 +0300

    Add hook to car spec

--- car_project/spec/car_spec.rb
+++ car_project/spec/car_spec.rb
@@ -8,28 +8,29 @@ describe 'Car' do
 
   # test attributes
   describe 'attributes' do
+    # create hook
+    before(:example) do # before 'it' blocks
+      @car = Car.new
+    end
+
     # skip example by xit
     xit 'allows reading and writing for :make' do
-      car = Car.new
-      car.make = 'Test'
+      @car.make = 'Test'
       expect(car.make).to eq('Test')
     end
 
     it 'allows reading and writing for :year' do
-      car = Car.new
-      car.year = 9999
-      expect(car.year).to eq(9999)
+      @car.year = 9999
+      expect(@car.year).to eq(9999)
     end
 
     it 'allows reading and writing for :color' do
-      car = Car.new
-      car.color = 'foo'
-      expect(car.color).to eq('foo')
+      @car.color = 'foo'
+      expect(@car.color).to eq('foo')
     end
 
     it 'allows reading for :wheels' do
-      car = Car.new
-      expect(car.wheels).to eq(4)
+      expect(@car.wheels).to eq(4)
     end
 
     # pending
